b5378547e86e7b248d454cf23880f13e418dd3de
Leonardo Gomes Batista <leonardog.bat@gmail.com>
2020-10-13 15:13:46 -0300

    Add more anime data fields and search by title

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const btn = document.getElementById('anime-search');
-btn.addEventListener('click', searchAnime);
+btn.addEventListener('click', searchAnime,false);
 
 function searchAnime() {
   const results = document.getElementById('results');
-  const input = document.getElementById('anime-input').value;
+  const inputValue = document.getElementById('anime-input').value;
 
-  const url = 'https://kitsu.io/api/edge/anime/' + input;
-  console.log(input);
+  const url = 'https://kitsu.io/api/edge/anime?filter[text]=' + inputValue;
+  console.log(inputValue);
   const xhr = new XMLHttpRequest();
   
   xhr.open('GET', url, true);
@@ -16,25 +16,31 @@ function searchAnime() {
       const resObj = JSON.parse(xhr.responseText);
       console.log(resObj);
   
-      const img = resObj.data.attributes.coverImage.original;
+      const { attributes } = resObj.data[0];
+      const img = attributes.coverImage && attributes.coverImage.original ? attributes.coverImage.original : attributes.posterImage.large;
+      const synopsis = attributes.synopsis;
+      const title = attributes.canonicalTitle;
+      const engTitle = attributes.titles.en;
+      const romTitle = attributes.titles.en_jp;
+      const jpTitle = attributes.titles.ja_jp;
+      const startDate = attributes.startDate;
+      const endDate = attributes.endDate;
+      const status = attributes.status;
+      const episodeCount = attributes.episodeCount;
+      const showType = attributes.showType;
       
-      console.log(img);
-      const text = resObj.data.attributes.synopsis;
-      const title = resObj.data.attributes.canonicalTitle;
-      
-      results.innerHTML = `<h3 class="mb-3">Anime: ${title}</h3>`;
-      results.innerHTML += `<img class="img-fluid" src="${img}" alt="${title}">`;
-      results.innerHTML += `<p class="mt-3">${text}</p>`;
+      results.innerHTML = `<h5 class="mb-0">Anime: ${title}</h5>`;
+      results.innerHTML += `<h6 class="mb-0 text-muted font-weight-light">Romanized: ${romTitle}</h6>`;
+      results.innerHTML += `<h6 class="mb-3 text-muted font-weight-light">Japanese: ${jpTitle}</h6>`;
+      results.innerHTML += `<img class="mb-3 img-fluid" src="${ img ? img : '' }" alt="${title}">`;
+      results.innerHTML += `<div class=""><strong>Type:</strong> ${showType}</div>`;
+      results.innerHTML += `<div class=""><strong>Episodes:</strong> ${episodeCount}</div>`;
+      results.innerHTML += `<div class=""><strong>Aired:</strong> ${startDate} ${endDate == null ? '' :  `to ${endDate}` }</div>`;
+      results.innerHTML += `<div class="mb-3"><strong>Status:</strong> ${status.toUpperCase()}</div>`;
+      results.innerHTML += `<h5 class="mb-2">Synopsis:</h5>`;
+      results.innerHTML += `<p class="mb-2">${synopsis}</p>`;
     }
   }
   
   xhr.send();
 }
-
-// const synopsis = data.attributes.synopsis;
-// const startDate = data.attributes.startDate;
-// const endDate = data.attributes.endDate;
-// const titles = data.attributes.titles;
-// const canonicalTitle = data.attributes.canonicalTitle;
-// const coverImage = data.attributes.coverImage.original;
-// const posterImage = data.attributes.posterImage.large;
